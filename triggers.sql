61238327b2f9df970ba6fb5014ec553bc85784d0
Alexander Yin <ayin005@bolt.cs.ucr.edu>
2021-03-09 16:45:30 -0800

    Added things to triggers.sql

---
+++ triggers.sql
@@ -0,0 +1,17 @@
+DROP SEQUENCE IF EXISTS part_numbers_seq;
+CREATE SEQUENCE part_numbers_seq START WITH 50000;
+DROP LANGUAGE IF EXISTS plpgsql;
+CREATE LANGUAGE plpgsql;
+
+CREATE OR REPLACE FUNCTION func_name()
+RETURNS "trigger" AS 
+$BODY$ 
+BEGIN
+INSERT INTO part_number VALUES nextval('part_numbers_seq');
+END;
+$BODY$
+LANGUAGE plpgsql VOLATILE;
+
+CREATE TRIGGER pop_part_num AFTER INSERT
+ON part_nyc FOR EACH ROW 
+EXECUTE PROCEDURE func_name()
