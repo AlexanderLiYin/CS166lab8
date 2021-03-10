b39d83c138c73639e8136697f6c15c2c14b9df9d
Alexander Yin <ayin005@bolt.cs.ucr.edu>
2021-03-10 12:22:21 -0800

    triggers now compiles and runs

--- triggers.sql
+++ triggers.sql
@@ -1,13 +1,14 @@
 DROP SEQUENCE IF EXISTS part_numbers_seq;
 CREATE SEQUENCE part_numbers_seq START WITH 50000;
-DROP LANGUAGE IF EXISTS plpgsql;
+DROP LANGUAGE IF EXISTS plpgsql CASCADE;
 CREATE LANGUAGE plpgsql;
 
 CREATE OR REPLACE FUNCTION func_name()
 RETURNS "trigger" AS 
 $BODY$ 
 BEGIN
-INSERT INTO part_number VALUES nextval('part_numbers_seq');
+New.part_number = nextval('part_numbers_seq');
+return NEW;
 END;
 $BODY$
 LANGUAGE plpgsql VOLATILE;
